01b7e9c134fd56728b748647f166fe054dd4cfcf
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-09-15 17:46:49 +0900

    Initial Commit

---
+++ SampleRikutoSatoSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleRikutoSatoSwiftUI
+//
+//  Created by mtanaka on 2022/09/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleRikutoSatoSwiftUI/SampleRikutoSatoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleRikutoSatoSwiftUIApp.swift
+//  SampleRikutoSatoSwiftUI
+//
+//  Created by mtanaka on 2022/09/15.
+//
+
+import SwiftUI
+
+@main
+struct SampleRikutoSatoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
